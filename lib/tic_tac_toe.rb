e9049118c279fa263f425bf8b3ae08b3f79da930
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-11-19 04:50:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,102 +1,46 @@
 class TicTacToe
-  attr_accessor :board
-  WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]
-  ]
-
   def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
-
-  def play
-    while !over?
-      turn
-    end
-    if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cat's Game!"
-    end
-  end
-
+  WIN_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]
   def display_board
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-
-  def turn
-      puts "Please enter 1-9:"
-      input = gets.strip
-      index = input.to_i
-      if !valid_move?(input)
-        turn
-      end
-      move(index, current_player)
-      display_board
-  end
-
-  def valid_move?(index)
-	    index = index.to_i - 1
-      index.between?(0,8) && !position_taken?(index.to_i)
-end
-
-  def current_player
-    turn_count.even? ? "X" : "O"
-  end
-
-  def full?
-    @board.all?{|token| token == "X" || token == "O"}
-  end
-
-  def over?
-    won? || draw?
-  end
-
-  def turn_count
-    @board.count{|token| token == "X" || token == "O"}
-  end
-
-  def move(location, token)
-    @board[location.to_i-1] = token
-  end
-
-def input_to_index(user_input)
-  user_input.to_i-1
-end
-
-  def won?
-    WIN_COMBINATIONS.detect do |combo|
-      position(combo[0]) == position(combo[1]) &&
-      position(combo[1]) == position(combo[2]) &&
-      position_taken?(combo[0])
-    end
-  end
-
-  def draw?
-    !won? && @board.all?{|token| token == "X" || token == "O"}
-  end
-
-  def winner
-    if winning_combo = won?
-      @winner = position(winning_combo.first)
-    end
-  end
-
-  def position(location)
-    @board[location.to_i]
-  end
-
-  def position_taken?(location)
-    !(position(location).nil? || position(location) == " ")
-  end
-end
+  def input_to_index(user_input)
+   user_input.to_i - 1
+ end
+ def move(index, token)
+   @board[index] = token
+ end
+ def position_taken?(index)
+   @board[index] != " "
+ end
+ def valid_move?(index)
+   !position_taken?(index) && index.between?(0,8)
+ end
+ def turn_count
+   @board.count{|square| square != " "}
+ end
+ def current_player
+   turn_count.even? ? "X" : "O"
+ end
+ def turn
+   puts "Please enter a number (1-9):"
+   user_input = gets.strip
+   index = input_to_index(user_input)
+   if valid_move?(index)
+     token = current_player
+     move(index, token)
+   else
+     turn
+   end
+   display_board
+ end
